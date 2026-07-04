e882f867fddffca6d99a18de3532cb94240264d6
asday <sara.and.zuka@gmail.com>
2026-07-04 19:34:21 +0100

    Look at that, some consistency, some validation.  Ace.

--- settings.py
+++ settings.py
@@ -5,3 +5,7 @@ import pathlib
 STATE_DIR = pathlib.Path(os.environ["STATE_DIRECTORY"])
 TOKEN = os.environ['SHUFFLE_TOKEN']
 CHANNEL = os.environ["SHUFFLE_WHO_UP_CHANNEL"]
+GROUP_SIZE = int(os.environ["SHUFFLE_GROUP_SIZE"])
+
+if GROUP_SIZE > 8:
+    raise ValueError("dingus the maximum people in a group slack DM is 8")
