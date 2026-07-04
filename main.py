8bbbc5ac09c50e7ad3cc56912ea59ac192962384
asday <sara.and.zuka@gmail.com>
2026-07-04 18:20:06 +0100

    Parsed Slack's insane nonsense.
    Seriously bro just send JSON.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import json
 import os
+import typing
 
 import fastapi
+import pydantic
 
 
 app = fastapi.FastAPI()
@@ -13,5 +16,29 @@ def task():
 @app.get("/")
 def home(background_tasks: fastapi.BackgroundTasks):
     background_tasks.add_task(task)
+
+    return
+
+
+class User(pydantic.BaseModel):
+    username: str
+
+
+class Action(pydantic.BaseModel):
+    action_id: str
+
+
+class BlockActions(pydantic.BaseModel):
+    type_: typing.Literal["block_actions"] = pydantic.Field(..., alias="type")
+    user: User
+    actions: pydantic.conlist(Action, min_length=1)
+
+
+@app.post("/interactivity")
+async def slack_callback(request: fastapi.Request):
+    async with request.form() as form:
+        data = json.loads(form["payload"])  # Slack is insane.
+
+    ba = BlockActions(**data)
 
     return
