57ae12be9d31fb4334cd83ce699cb75ff5e532d8
asday <sara.and.zuka@gmail.com>
2026-07-16 15:08:07 +0900

    Assertions using assert.

--- settings.py
+++ settings.py
@@ -7,5 +7,4 @@ TOKEN = os.environ['SHUFFLE_TOKEN']
 CHANNEL = os.environ["SHUFFLE_WHO_UP_CHANNEL"]
 GROUP_SIZE = int(os.environ["SHUFFLE_GROUP_SIZE"])
 
-if GROUP_SIZE > 8:
-    raise ValueError("dingus the maximum people in a group slack DM is 8")
+assert GROUP_SIZE <= 8, "dingus the maximum people in a group slack DM is 8"
