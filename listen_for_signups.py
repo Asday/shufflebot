477fa5be3dd5b84e85e54d9bb8c610c3708cad8c
asday <sara.and.zuka@gmail.com>
2026-07-23 16:47:10 +0900

    Removed old testing code.

--- listen_for_signups.py
+++ listen_for_signups.py
@@ -12,15 +12,6 @@ import settings
 app = fastapi.FastAPI()
 
 
-def task():
-    print(os.environ.get("SLACK_APP_ID"))
-
-
-@app.get("/")
-def home(background_tasks: fastapi.BackgroundTasks):
-    background_tasks.add_task(task)
-
-
 class User(pydantic.BaseModel):
     id_: str = pydantic.Field(..., alias="id")
 
